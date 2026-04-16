68656ebb7e5f14ccff72f6af50deeca3ac672e8f
Yurupari <cristhianmarin15@gmail.com>
2026-04-16 13:49:34 +0200

    cristhian: alert-service created

---
+++ alert-service/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+	java
+}
+
+dependencies {
+	implementation(project(":common-data"))
+	implementation("org.springframework.boot:spring-boot-starter-kafka")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-kafka-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-mail-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testImplementation("com.h2database:h2")
+	testCompileOnly("org.projectlombok:lombok")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testAnnotationProcessor("org.projectlombok:lombok")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ alert-service/src/test/java/com/yurupari/alert_service/AlertServiceApplicationTests.java
@@ -0,0 +1,19 @@
+package com.yurupari.alert_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@Sql(scripts = {"/sql/cleanup.sql", "/sql/insert.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+class AlertServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
