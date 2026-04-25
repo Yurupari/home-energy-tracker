f6ebcca8a6491b36b810c5adb3bcb67ab811296b
Yurupari <cristhianmarin15@gmail.com>
2026-04-25 10:28:07 +0200

    cristhian: added unit tests to api-gateway

---
+++ api-gateway/src/test/java/com/yurupari/api_gateway/route/InsightServiceRouteTest.java
@@ -0,0 +1,82 @@
+package com.yurupari.api_gateway.route;
+
+import com.yurupari.api_gateway.config.GatewayProperties;
+import com.yurupari.api_gateway.model.ServiceConfig;
+import com.yurupari.api_gateway.route.factory.GatewayRouteFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.servlet.function.RouterFunction;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class InsightServiceRouteTest {
+
+    private InsightServiceRoute insightServiceRoute;
+
+    private GatewayProperties properties;
+
+    @Mock
+    private GatewayRouteFactory routeFactory;
+
+    @BeforeEach
+    void setUp() {
+        Map<String, ServiceConfig> propertyMap = new HashMap<>();
+        propertyMap.put("insight-service", new ServiceConfig(
+                "http://localhost:9999",
+                "/api/v1/insight/**",
+                "insightServiceCircuitBreaker",
+                "/fallback/insight",
+                "Insight service is down"
+        ));
+
+        properties = new GatewayProperties(propertyMap);
+
+        insightServiceRoute = new InsightServiceRoute(properties, routeFactory);
+    }
+
+    @Test
+    void testInsightRoute() {
+        var routerFunction = mock(RouterFunction.class);
+        when(routeFactory.createServiceRoute(anyString(), anyString(), anyString(), anyString(), anyString())).thenReturn(routerFunction);
+
+        var response = insightServiceRoute.insightRoute();
+
+        assertNotNull(response);
+        assertEquals(routerFunction, response);
+        verify(routeFactory).createServiceRoute(
+                eq("insight-service"),
+                eq("/api/v1/insight/**"),
+                eq("http://localhost:9999"),
+                eq("insightServiceCircuitBreaker"),
+                anyString()
+        );
+    }
+
+    @Test
+    void testInsightFallbackRoute() {
+        var routerFunction = mock(RouterFunction.class);
+        when(routeFactory.createFallbackRoute(anyString(), anyString())).thenReturn(routerFunction);
+
+        var response = insightServiceRoute.insightFallbackRoute();
+
+        assertNotNull(response);
+        assertEquals(routerFunction, response);
+        verify(routeFactory).createFallbackRoute(
+                anyString(),
+                eq("/fallback/insight")
+        );
+    }
+}
